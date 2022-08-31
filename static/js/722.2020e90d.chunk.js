bd4fea01c24a688967afe03eeba3cb870bff13e7
Holms85 <94076735+Holms85@users.noreply.github.com>
2022-08-31 19:16:51 +0000

    Deploying to gh-pages from @ Holms85/goit-react-hw-05-movies@a43a9edeabfc2828f8ccd5d85d2eab7792d7bd9c 🚀

---
+++ static/js/722.2020e90d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[722],{3722:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var r=t(501),a=t(184);function c(){return(0,a.jsxs)("div",{className:"container",children:[(0,a.jsx)("h1",{children:"Page not found"}),(0,a.jsx)(r.rU,{to:"/",children:"Back to Main page"})]})}}}]);
+//# sourceMappingURL=722.2020e90d.chunk.js.map
